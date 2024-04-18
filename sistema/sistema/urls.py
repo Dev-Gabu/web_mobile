ae4b332dd53d87d2faa2363107b3e2fb3d8417ab
Dev-Gabu <gabrieltds83@gmail.com>
2024-04-17 21:40:25 -0300

    Atualizações do sistema dia 17/04

--- sistema/sistema/urls.py
+++ sistema/sistema/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from sistema.views import Login
+from sistema.views import Login, Logout
 
 urlpatterns = [
     path('', Login.as_view(), name='index'),
     path('admin/', admin.site.urls),
+    path('logout', Logout.as_view(),name='logout')
 ]
