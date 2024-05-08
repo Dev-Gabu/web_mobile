7b79e75541992ee02158be07636b4595687049aa
Dev-Gabu <gabrieltds83@gmail.com>
2024-05-08 18:13:32 -0300

    Atualização aula 24/03

--- sistema/veiculo/views.py
+++ sistema/veiculo/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import View
+from veiculo.models import Veiculo
 
-# Create your views here.
+class ListarVeiculos(View):
+
+    def get(self, request):
+        contexto = {
+            'veiculos' : Veiculo.objects.all().order_by('marca')
+        }
+
+        return render(request, 'veiculo/listar.html', contexto)
