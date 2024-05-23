4433e5159bab885a31958662350917f6b37c1bf1
Dev-Gabu <gabrieltds83@gmail.com>
2024-05-23 19:19:54 -0300

    Atualização aula 22/05

--- sistema/veiculo/urls.py
+++ sistema/veiculo/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from veiculo.views import ListarVeiculos, FotoVeiculo, CriarVeiculos, EditarVeiculos
+from veiculo.views import ListarVeiculos, FotoVeiculo, CriarVeiculos, EditarVeiculos, DeletarVeiculos, APIListarItens
 
 urlpatterns = [
     path('', ListarVeiculos.as_view(), name='listar-veiculos'),
     path('novo/', CriarVeiculos.as_view(), name = 'criar-veiculos'),
+    path('api/', APIListarItens.as_view(),name='api-listar-veiculos'),
     path('<int:pk>/', EditarVeiculos.as_view(), name = 'editar-veiculos'),
+    path('deletar/<int:pk>', DeletarVeiculos.as_view(), name = 'deletar-veiculos'),
     path('fotos/<str:arquivo>/', FotoVeiculo.as_view(), name='foto-veiculo')
 ]
--- sistema/veiculo/views.py
+++ sistema/veiculo/views.py
@@ -2,8 +2,12 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import FileResponse, Http404
 from django.shortcuts import render
-from django.views.generic import View, ListView, CreateView, UpdateView
+from django.views.generic import View, ListView, CreateView, UpdateView, DeleteView
 from veiculo.models import Veiculo
+from veiculo.serializers import SerializadorVeiculo
+from rest_framework import permissions
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.generics import ListAPIView
 from veiculo.forms import FormularioVeiculo
 from django.urls import reverse_lazy
 
@@ -12,6 +16,18 @@ class ListarVeiculos(ListView):
     model = Veiculo
     context_object_name = 'veiculos'
     template_name = 'veiculo/listar.html'
+
+    def get_queryset(self, **kwargs):
+        queryset =  Veiculo.objects.all()
+        pesquisa = self.request.GET.get('pesquisa', None)
+        if pesquisa is not None:
+            query = queryset.filter(modelo_icontains=pesquisa)
+        return queryset
+    
+class APIListarItens(ListAPIView):
+    serializer_class = SerializadorVeiculo
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
     
 class FotoVeiculo(View):
 
@@ -36,4 +52,9 @@ class EditarVeiculos(LoginRequiredMixin, UpdateView):
     model = Veiculo
     form_class = FormularioVeiculo
     template_name = 'veiculo/editar.html'
+    success_url = reverse_lazy('listar-veiculos')
+
+class DeletarVeiculos(LoginRequiredMixin, DeleteView):
+    model = Veiculo
+    template_name = 'veiculo/deletar.html'
     success_url = reverse_lazy('listar-veiculos')
